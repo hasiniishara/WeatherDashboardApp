7527e6a17d5bcbdda883b1eea01a83c9f1aa28ae
HasiniIshara <hasiniisharao4@gmail.com>
2024-03-06 13:20:09 +0200

    Implementing weather app.

--- weatherHomeBack.js
+++ weatherHomeBack.js
@@ -15,32 +15,32 @@ document.getElementById('search-button').addEventListener('click', function(){
 })
 
 
-// function displayWeather (message, sender){
+function displayWeather (weather){
     
+    console.log(`weather is ${weather.name}`);
     
-//     const cityW = document.getElementById('city-name');
-//     const temperatureW = document.getElementById('city-name');
-//     const humidityW = document.getElementById('city-name');
-//     const windspeedW = document.getElementById('city-name');
+    const cityW = document.getElementById('city-name');
+    const temperatureW = document.getElementById('temperature');
+    const humidityW = document.getElementById('humidity');
+    const windspeedW = document.getElementById('wind-speed');
 
-//     cityW.innerText ="";
-//     temperatureW.innerText ="";
-//     humidityW.innerText ="";
-//     windspeedW.innerText ="";  
+    cityW.innerText =`City: ${weather.name}`;
+    temperatureW.innerText =`Temperature: ${weather.main.temp}`;
+    humidityW.innerText =`Humidity: ${weather.main.humidity}`;
+    windspeedW.innerText =`Wind Speed: ${weather.wind.speed}`;  
 
-// }
+}
 
 async function fetchChatResponse (userSearch){
     console.log(userSearch);
     const apiKey = '454dcf8be544f4822cb24cfead8501ec';
 
     try{
-        const url = 'https://api.openweathermap.org/data/2.5/weather?q=${userSearch}&appid=${apiKey}&units=metric';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${userSearch}&appid=${apiKey}&units=metric`;
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
-       // const replyFromBot =data.choices[0].message.content.trim();
-       // displayMessage(replyFromBot, 'bot')
+       displayWeather(data);
     }catch (error){
         console.log('Error', error);
     }
